79d6b47f71e52a725ebdd0bd62d8c4143f2ad78c
Darknessson69008 <131368078+Darknessson69008@users.noreply.github.com>
2023-07-31 18:49:14 +0200

    modified:   public/index.html 	modified:   src/pages/Error/error.jsx

--- src/pages/Error/error.jsx
+++ src/pages/Error/error.jsx
@@ -5,7 +5,7 @@ function Error() {
   return (
     <div className="error">
       <h1 className="error__title">404</h1>
-      <h2 className="error__message">Oups! La page que vous demandez n'existe pas.</h2>
+      <h2 className="error__message">La page que vous demandez n'existe pas.</h2>
       <Link to='/' className="error__link">Retour à la page d'accueil</Link>
       
     </div>
